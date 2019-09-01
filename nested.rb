8624c7fb5546c50d86d8071023203732c8e0a67a
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-01 22:54:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,6 +105,7 @@ def changing_alan
         }
      }
      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
