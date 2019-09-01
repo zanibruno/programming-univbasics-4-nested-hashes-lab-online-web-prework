f4475a715733897e121a85c351428bdccb6bd3a8
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-01 22:58:53 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper] 
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0] 
 end
 
 def adding_matz
@@ -66,6 +68,7 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		 
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,6 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP" , "C"]}
+     return programmer_hash
 
     
 end
@@ -99,6 +104,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     return programmer_hash
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
@@ -121,5 +128,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+     return programmer_hash
 end
