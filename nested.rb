b936e79d028dff885298ee80eddb4b5d17b1723a
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-01 22:58:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages] + "Assembly"
+     programmer_hash[:dennis_ritchie][:languages]["Assembly"]
      return programmer_hash
 end
