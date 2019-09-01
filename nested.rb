b94506ec52c2da0116d48f7a432ff4f172a70e67
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-01 22:38:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP" , "C"]}
+     programmer_hash.unshift[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP" , "C"]}
      
 
     
